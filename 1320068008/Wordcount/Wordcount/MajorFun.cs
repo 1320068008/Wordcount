bbaac82643912db89d586844df47e99fe7d63a29
1320068008 <1320068008@qq.com>
2019-04-05 14:25:19 +0800

    Second Commit

--- 1320068008/Wordcount/Wordcount/MajorFun.cs
+++ 1320068008/Wordcount/Wordcount/MajorFun.cs
@@ -12,22 +12,20 @@ namespace Wordcount
     class MajorFun
     {
         byte[] data;
-        long length;
         string path;
-        public MajorFun(long length,byte [] data,string path)
+        public MajorFun(byte [] data,string path)
         {
-            this.data = new byte[length];
             this.data = data;
-            this.length = length;
             this.path = path;
         }  
         ArrayList list = new ArrayList();
+        List<string> Cout = new List<string>();
         long Count1 = 0;
         //此函数用于计算文本Ascii码字符个数
         public void CountChar()
         {
             long Count=0;
-            for(long i=0;i<length;i++)
+            for(long i=0;i<data.Length;i++)
             {
                 if (data[i] >= 0 && data[i] <= 127&&data[i]!='\n')
                 {
@@ -38,14 +36,19 @@ namespace Wordcount
                     Count1++;
                 }
             }
+            long c = Count - Count1;
+            Cout.Add("字符个数："+c);
             Console.WriteLine("字符个数："+(Count-Count1));
         }
         //此函数用于计算统计出现次数在前十的单词
-       public void CountE_word(string path, long n)
+       public void CountE_word(string path, int m,int n)
         {
             List<string> list = new List<string>();
+            List<string> list1 = new List<string>();
             Dictionary<string, int> frequencies = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+            Dictionary<string, int> frequencies1 = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
             string s = "";
+            string s1 = "";
             if (File.Exists(path))
             {
                 StreamReader sr = new StreamReader(path, Encoding.Default);
@@ -58,15 +61,16 @@ namespace Wordcount
                         if (Regex.IsMatch(word, "^[a-zA-Z]{4,}[a-zA-Z0-9]*"))
                         {
                             list.Add(word);
+                            list1.Add(word);
                         }
                     }
                 }
                 sr.Close();
             }
-            for (int i = 0; i <= list.Count - n; i++)
+            for (int i = 0; i <= list.Count - 1; i++)
             {
                 int j;
-                for (s = list[i], j = 0; j < n - 1; j++)
+                for (s = list[i], j = 0; j < 0; j++)
                 {
                     s += " " + list[i + j + 1];
                 }
@@ -77,18 +81,44 @@ namespace Wordcount
                 else
                     frequencies[s] = 1;
             }
-            Dictionary<string, int> item = frequencies.OrderByDescending(r => r.Value).ThenBy(r => r.Key).ToDictionary(r => r.Key, r => r.Value);
             int o = 0;
-            Console.WriteLine("单词频率前十名及其出现的次数：");
-            foreach (var dic in item)
+            Console.WriteLine("单词频率前"+n+"名及其出现的次数：");
+            Dictionary<string, int> item = frequencies.OrderByDescending(r => r.Value).ThenBy(r => r.Key).ToDictionary(r => r.Key, r => r.Value);
+            foreach (KeyValuePair<string, int> entry in item)
             {
                 o++;
-                if (o > 10)
+                if (o > n)
                     break;
-                Console.Write("No.{0}: {1}  {2}次\n",o,dic.Key,dic.Value );    
+                string word = entry.Key;
+                int frequency = entry.Value;
+                Console.WriteLine("No." + o+" "+ word + "  :  " + frequency+"次 ");
+                Cout.Add("No." + o+" "+ word + "  :  " + frequency+"次 ");
+            }     
+            for (int i = 0; i <= list1.Count - m; i++)
+            {
+                int j;
+                for (s1 = list1[i], j = 0; j < m - 1; j++)
+                {
+                    s1 += " " + list1[i + j + 1];
+                }
+                if (frequencies1.ContainsKey(s1))
+                {
+                    frequencies1[s1]++;
+                }
+                else
+                    frequencies1[s1] = 1;
+            }
+            Dictionary<string, int> item1 = frequencies1.OrderByDescending(r => r.Value).ThenBy(r => r.Key).ToDictionary(r => r.Key, r => r.Value);
+            Console.WriteLine("输出长度为" + m + "的单词组：");
+            Cout.Add("输出长度为" + m + "的单词组：");
+            foreach (var dic1 in item1)
+            {
+                Console.Write(" {0}  {1}次\n",dic1.Key,dic1.Value );
+                Cout.Add(dic1.Key.ToString()+"  "+dic1.Value+"次");
             }
             Console.WriteLine("单词总数：");
             Console.WriteLine("{0}个",list.Count );
+            Cout.Add("单词总数："+list.Count);
         }
         //此函数用于计算文本有效行数
         public void GetRows()
@@ -107,11 +137,33 @@ namespace Wordcount
                         }
                     }
                     Console.WriteLine("行数 ："+Countline);
+                    Cout.Add("行数 ："+Countline);
+
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
+            }
+        }
+        //此函数用于将屏幕上输出内容写入指定文件
+        public void  WritoFile(string path)
+        {
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("路径不存在！");
+            }
+            else
+            {
+                FileStream fs = new FileStream(path,FileMode.Create, FileAccess.Write);
+                StreamWriter sr = new StreamWriter(fs);
+                foreach(string w in Cout)
+                {
+                    sr.WriteLine(w);
+                }
+                Console.WriteLine("写入成功");
+                sr.Close();
+                fs.Close();
             }
         }
     }
--- 1320068008/Wordcount/Wordcount/Program.cs
+++ 1320068008/Wordcount/Wordcount/Program.cs
@@ -17,7 +17,7 @@ namespace Wordcount
             path = Console.ReadLine();
         }
         //此函数用于读取文本文件内容
-        public void ReadFile(string path)
+        public byte[] ReadFile(string path)
         {
               if (File.Exists(path))
               {
@@ -25,17 +25,22 @@ namespace Wordcount
                 zipdata = new byte[fs.Length];
                 fs.Read(zipdata, 0, zipdata.Length);
                 fs.Close();
+                return zipdata;
               }
               else
+              {
                 Console.WriteLine("路径不存在！");
+                return zipdata;
+              }
         }
         //此函数用于调用主功能函数
-        public void Movedata()
+        public void Movedata(int m,int n,string p)
         {
-            MajorFun Maj = new MajorFun(zipdata.Length, zipdata,path );
+            MajorFun Maj = new MajorFun(zipdata,path );
             Maj.CountChar();
-            Maj.CountE_word(path,1);
+            Maj.CountE_word(path,m,n);
             Maj.GetRows();
+            Maj.WritoFile(p);
         }      
     }
     class Program
@@ -43,10 +48,79 @@ namespace Wordcount
         static void Main(string[] args)
         {
             FileOp f = new FileOp();
-            f.GetPath();
-            f.ReadFile(f.path );
-            f.Movedata();
-            Console.ReadKey();
+            int m=1, n=10;
+            int i = 0;
+            string[] arg;
+            arg = Sort(args);
+            if (arg.Length != 0)
+            {
+                for (i = 0; i < arg.Length; i++)
+                {
+                    if (arg[i] == "-m")
+                    {
+                        m = int.Parse(arg[i + 1]);
+                    }
+                    if (arg[i] == "-n")
+                    {
+                        n = int.Parse(arg[i + 1]);
+                    }
+                }
+                f.path = arg[1];
+                f.ReadFile(f.path);
+                f.Movedata(m, n, arg[arg.Length - 1]);
+                Console.ReadKey();
+                }
+            else
+            {
+                Console.WriteLine("缺少有效读入或输出文件路径或命令指示标志");
+            }
+            
+        }
+        static string [] Sort(string[] w)
+        {
+            int i=0;
+            int count=0;
+            while(i<w.Length)
+            {
+                if(w[i]=="-i"||w[i]=="-o")
+                {
+                    count++;
+                }
+                i++;
+            }
+            if(count==2)
+            {
+                string tempO,tempS;
+                for(i=0;i<w.Length;i++)
+                {
+                    if(w[i]=="-i")
+                    {
+                        
+                        tempO = w[i];
+                        tempS = w[i + 1];
+                        w[i] = w[0];
+                        w[i + 1] = w[1];
+                        w[0] = tempO;
+                        w[1] = tempS;
+                    }
+                    if(w[i]=="-o")
+                    {
+                        tempO = w[i];
+                        tempS = w[i + 1];
+                        w[i] = w[w.Length-2];
+                        w[i + 1] = w[w.Length-1];
+                        w[w.Length - 2] = tempO;
+                        w[w.Length - 1] = tempS;
+                    }
+                }
+                return w;
+            }
+            else
+            {
+                string[] x = new string[0];
+                return x;
+            }
+            
         }
     }
 }
